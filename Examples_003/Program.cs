703dcfc8abd0e324f62ce85473fb791f5bdfba85
igor <igoshinig@yandex.ru>
2022-06-23 16:12:58 +0500

    new Random

--- Examples_003/Program.cs
+++ Examples_003/Program.cs
@@ -1,5 +1,11 @@
-﻿int numberA = 45;
-int numberB = 23;
-//Console.WriteLine(numberA + numberB);
+﻿//int numberA = 45;
+//int numberB = 23;
+//Console.WriteLine(numberA + numberB); первый вариан 
+//int result = numberA + numberB; второй вариан
+//Console.WriteLine(result);
+int numberA = new Random() .Next(1, 10); // 1 2 3 4...9
+Console.WriteLine(numberA);
+int numberB = new Random() .Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
