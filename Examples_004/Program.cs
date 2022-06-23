3fc5792f75a850c8a4b3b1973937a164170a3f80
igor <igoshinig@yandex.ru>
2022-06-23 16:24:33 +0500

    Examples_004 division

--- Examples_004/Program.cs
+++ Examples_004/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//int numberA = 22; работа в целых числах 4,4 округляет к 4.
+//int numberB = 5;
+double numberA = 22;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
