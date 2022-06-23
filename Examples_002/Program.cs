0e7b9d3c4103ffabea9784e7f0c75f77e8634f84
igor <igoshinig@yandex.ru>
2022-06-23 14:25:07 +0500

    Examples_002 hello user

---
+++ Examples_002/Program.cs
@@ -0,0 +1,8 @@
+﻿//Console.WriteLine("Ведите ваше имя ");
+//string username = Console.ReadLine();
+//Console.WriteLine("Приве, ");
+//Console.WriteLine(username);
+Console.Write("Ведите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Приве, ");
+Console.Write(username);
