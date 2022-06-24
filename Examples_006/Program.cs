9ada2c4b64731727e1263fa501995a6d2175e36d
igor <igoshinig@yandex.ru>
2022-06-24 12:04:37 +0500

    Задача с гирями.

---
+++ Examples_006/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача с гирями.
+/*Дано: 5 гирь с разным весом в случайном порядке.
+Найти: вес самой тяжелой гири.*/
+
+int a = 1;
+int b = 8;
+int c = 3;
+int d = 2;
+int e = 6;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
